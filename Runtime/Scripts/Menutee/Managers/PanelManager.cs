d20e62658ba730c3768ec76d82a9ef6dcf57a6d6
Kevin Hutchins <possessedcow@gmail.com>
2021-09-10 17:34:10 -0700

    Check for PanelManager before creating it on panel prefabs, allow it to designate a parent object for panel objects.

--- Runtime/Scripts/Menutee/Managers/PanelManager.cs
+++ Runtime/Scripts/Menutee/Managers/PanelManager.cs
@@ -2,8 +2,14 @@
 
 namespace Menutee {
     public class PanelManager : MonoBehaviour {
+        [Tooltip("Parent object that panel objects will be placed in. If null, will use this game object.")]
+        public GameObject Parent;
+
+        [HideInInspector]
         public string Key;
+        [HideInInspector]
         public UIElementManager DefaultInput;
+        [HideInInspector]
         public GameObject[] OtherObjects;
 
         public void SetPanelActive(bool active) {
