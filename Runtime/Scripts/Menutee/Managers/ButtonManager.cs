e2bf6718cb2aae8eb76cce8b31cd51859391761b
Kevin Hutchins <possessedcow@gmail.com>
2022-01-15 13:04:54 -0800

    No longer require button to be on the ButtonManager script object but automatically retrieve it if it is there. Adds button with image (the standard Unity button) configured for use with Menutee.

--- Runtime/Scripts/Menutee/Managers/ButtonManager.cs
+++ Runtime/Scripts/Menutee/Managers/ButtonManager.cs
@@ -6,16 +6,19 @@ using UnityEngine.UI;
 
 namespace Menutee {
     public delegate void ButtonPressedHandler(ButtonManager button);
-    [RequireComponent(typeof(Button))]
     public class ButtonManager : UIElementManager {
         public int Id;
         public TextMeshProUGUI Text;
+        [Tooltip("Button this manager manages. If null, will attempt to retrieve it from this object.")]
+        public Button Button;
 
         public event ButtonPressedHandler ButtonPressed;
 
         void Awake() {
-            Button button = GetComponent<Button>();
-            button.onClick.AddListener(ButtonWasPressed);
+            if (Button == null) {
+                Button = GetComponent<Button>();
+            }
+            Button.onClick.AddListener(ButtonWasPressed);
         }
 
         public void SetText(string newText) {
